1a85d709245c26fff0832c9eec9da0cd2dda8645
kirantrainings <pvskiran.trainer@gmail.com>
2016-03-25 11:00:02 +0530

    OOJS

---
+++ OOJS/OOJS/app/register.js
@@ -0,0 +1,23 @@
+﻿function Person(firstName,lastName,age,dob,gender,ssn) {
+    var SSN = ssn;
+    var dob = dob;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.gender = gender;
+    this.last4DigitsSSN = function () {
+        return SSN.substring(6, 10);
+    }
+}
+
+function printDetails() {
+    var student = new Person("kiran","PVS","30","18/12/1981","M","1234567890");
+    console.log(student.firstName);
+    console.log(student.lastName);
+    console.log(student.age);
+    console.log(student.SSN);//SSN is a private variable.
+    console.log(student.last4DigitsSSN());
+}
+
+printDetails();
+
